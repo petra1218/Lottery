68ef50792608ce3daf11853e9990eaa66e14c48d
petra <petra1218@gmail.com>
2017-11-14 21:33:05 +0800

    恢复

---
+++ app/src/main/java/com/petra/lottery/MyApplication.java
@@ -0,0 +1,14 @@
+package com.petra.lottery;
+
+import android.app.Application;
+
+/**
+ * Created by qiang on 2017/11/4.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
